b2f172e6392ee84194317f103d70e2e8daa1ba82
darshanmarathe <darshanmarathe@gmail.com>
2022-03-27 18:15:36 +0530

    code push

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const path = require('path');
 
+const package = require('./package.json');
+console.log(package.proxy)
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname , 'build/index.html'))
 })
