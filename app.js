ccb531dd8a371209772693d28ae4218b6f925eef
darshanmarathe <darshanmarathe@gmail.com>
2022-03-27 17:15:14 +0530

    code added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname , 'build/index.html'))
 })
 
+app.use(express.static(path.join(__dirname , 'build')))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
